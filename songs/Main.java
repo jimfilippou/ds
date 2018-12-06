9174c90e545b00edcd46261771f63c2d241032ba
MolwnVele <nainaioxioxi@yahoo.gr>
2018-12-06 14:59:08 +0200

    add song add main

--- songs/Main.java
+++ songs/Main.java
@@ -1,5 +1,34 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 class Main {
-    public static void main (String[] args){
-        System.out.print("hey");
+    public static void main(String[] args) {
+        
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("songs.txt"));
+            try {
+                StringBuilder sb = new StringBuilder();
+                try {
+                    String line = br.readLine();
+                    while (line != null) {
+                        sb.append(line);
+                        sb.append(System.lineSeparator());
+                        line = br.readLine();
+                    String everything = sb.toString();
+                    System.out.println(line);
+                }
+                } catch (Exception e) {
+                    //TODO: handle exception
+                }
+            } finally {
+                br.close();
+            }
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+        
+        
     }
 }
---
+++ songs/Song.java
@@ -0,0 +1,14 @@
+class Song{
+    int id;
+    String title;
+    int likes;
+
+    public Song(int id, String title, int likes){
+
+        this.id=id;
+        this.title=title;
+        this.likes=likes;
+
+    }
+
+}
