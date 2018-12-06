0827c8faf11932f658a2ad6beecd333ef2081920
MolwnVele <nainaioxioxi@yahoo.gr>
2018-12-06 20:35:58 +0200

    πρεπει να κανουμε το if και εξω απο το while για να γινει εστω μια φορα. το Id εξω απο το if καθως δεν μας ενδιαφερει ειναι στανταρ. αν ειναι 3 ξερουμε στανταρ θεση. αλλιως φορ για να γεμισουμε τις θεσεις μεχρι x-2

--- songs/Main.java
+++ songs/Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 class Main {
@@ -14,24 +15,47 @@ class Main {
                 StringBuilder sb = new StringBuilder();
                 try {
                     String line = br.readLine();
+                    String[] data = line.split(" ");
+                    System.out.println(data.length);
+                    if (data.length == 3) {
+                        String title = line.split(" ")[1];
+                        int likes = Integer.parseInt(line.split(" ")[2]);
+                    } else {
+                        for (int i = 1; i < (data.length - 2); i++) {
+                            String title = line.split(" ")[i];
+                        }
+                        System.out.println("run");
+                        int likes = Integer.parseInt(line.split(" ")[data.length - 1]);
+                    }
+
                     while (line != null) {
+                        String[] data2 = line.split(" ");
                         sb.append(line);
                         sb.append(System.lineSeparator());
                         line = br.readLine();
                         int id = Integer.parseInt(line.split(" ")[0]);
-                        String title = line.split(" ")[1];
-                        int likes = Integer.parseInt(line.split(" ")[2]);
-                        Song song = new Song(id, title, likes);
-                        list.add(song);
+                        if (data2.length == 3) {
+                            String title = line.split(" ")[1];
+                            int likes = Integer.parseInt(line.split(" ")[2]);
+                        } else {
+                            for (int i = 1; i < (data2.length - 2); i++) {
+                                String title = line.split(" ")[i];
+                            }
+                            System.out.println("run");
+                            int likes = Integer.parseInt(line.split(" ")[data.length - 1]);
+                        }
+                        System.out.println(data2.length);
+                        // Song song = new Song(id, title, likes);
+                        // list.add(song);
                     }
                 } catch (Exception e) {
                     // TODO: handle exception
                 }
             } finally {
                 br.close();
-                for (Song s : list) {
-                    System.out.println(s.title);
-                }
+                // for (Song s : list) {
+                // System.out.println(s.likes);
+                // }
             }
         } catch (Exception e) {
             // TODO: handle exception
