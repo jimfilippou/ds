326a84d08856868425fa04c8deb81aeba5b78459
jimfilippou <jimfilippou8@gmail.com>
2018-12-09 13:49:41 +0200

    fix songs add

--- songs/Main.java
+++ songs/Main.java
@@ -8,54 +8,41 @@ import java.util.ArrayList;
 class Main {
     public static void main(String[] args) {
         ArrayList<Song> list = new ArrayList<Song>();
-        list.add(new Song(114, "Fuego", 51));
         try {
             BufferedReader br = new BufferedReader(new FileReader("songs.txt"));
             try {
                 StringBuilder sb = new StringBuilder();
                 try {
-                    String line = br.readLine();
-                    String[] data = line.split(" ");
-                    System.out.println(data.length);
-                    if (data.length == 3) {
-                        String title = line.split(" ")[1];
-                        int likes = Integer.parseInt(line.split(" ")[2]);
-                    } else {
-                        for (int i = 1; i < (data.length - 2); i++) {
-                            String title = line.split(" ")[i];
-                        }
-                        System.out.println("run");
-                        int likes = Integer.parseInt(line.split(" ")[data.length - 1]);
-                    }
-
-                    while (line != null) {
-                        String[] data2 = line.split(" ");
-                        sb.append(line);
-                        sb.append(System.lineSeparator());
-                        line = br.readLine();
-                        int id = Integer.parseInt(line.split(" ")[0]);
-                        if (data2.length == 3) {
-                            String title = line.split(" ")[1];
-                            int likes = Integer.parseInt(line.split(" ")[2]);
+                    while (true) {
+                        String line = br.readLine();
+                        if (line == null)
+                            break;
+                        String[] data = line.split(" ");
+                        String title;
+                        int likes = Integer.parseInt(data[data.length - 1]);
+                        int id = Integer.parseInt(data[0]);
+                        if (data.length == 3) {
+                            title = data[1];
                         } else {
-                            for (int i = 1; i < (data2.length - 2); i++) {
-                                String title = line.split(" ")[i];
+                            for (int i = 1; i < data.length - 1; i++) {
+                                sb.append(data[i]);
+                                if (!(i == data.length - 2))
+                                    sb.append(" ");
                             }
-                            System.out.println("run");
-                            int likes = Integer.parseInt(line.split(" ")[data.length - 1]);
+                            title = sb.toString();
                         }
-                        System.out.println(data2.length);
-                        // Song song = new Song(id, title, likes);
-                        // list.add(song);
+                        sb = new StringBuilder();
+                        Song song = new Song(id, title, likes);
+                        list.add(song);
                     }
                 } catch (Exception e) {
                     // TODO: handle exception
                 }
             } finally {
                 br.close();
-                // for (Song s : list) {
-                // System.out.println(s.likes);
-                // }
+                for (Song s : list) {
+                    System.out.println(s.id + " " + s.title + " " + s.likes);
+                }
             }
         } catch (Exception e) {
             // TODO: handle exception
