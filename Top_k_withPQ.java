0f8179135849a04a5644212fe5232e02b8e255d2
jimfilippou <jimfilippou8@gmail.com>
2018-12-16 15:38:41 +0200

    added songs to PQ

--- Top_k_withPQ.java
+++ Top_k_withPQ.java
@@ -1,20 +1,51 @@
-
+import java.io.File;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Top_k_withPQ
  */
 public class Top_k_withPQ {
     public static void main(String[] args) {
-        System.out.println("yep");
         PQ<Song> pq = new PQ<Song>();
-        pq.insert(new Song(1, "a", 23));
-        System.out.println(pq.toString());
-        pq.insert(new Song(2, "b", 24));
-        System.out.println(pq.toString());
-    
-        pq.getMax();
-        System.out.println(pq.toString());
-        pq.getMax();
-        System.out.println(pq.toString());
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("songs.txt"));
+            try {
+                StringBuilder sb = new StringBuilder();
+                try {
+                    while (true) {
+                        String line = br.readLine();
+                        if (line == null)
+                            break;
+                        String[] data = line.split(" ");
+                        String title;
+                        int likes = Integer.parseInt(data[data.length - 1]);
+                        int id = Integer.parseInt(data[0]);
+                        if (data.length == 3) {
+                            title = data[1];
+                        } else {
+                            for (int i = 1; i < data.length - 1; i++) {
+                                sb.append(data[i]);
+                                if (!(i == data.length - 2))
+                                    sb.append(" ");
+                            }
+                            title = sb.toString();
+                        }
+                        sb = new StringBuilder();
+                        Song song = new Song(id, title, likes);
+                        pq.insert(song);
+                    }
+                } catch (Exception e) {
+                }
+            } finally {
+                br.close();
+                System.out.println(pq.toString());
+            }
+        } catch (Exception e) {
+        }
     }
 }
